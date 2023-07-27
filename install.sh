66a15426f5512f5dbce09a74366808a05db0e05f
Amina Anna <96390957+amin-nna@users.noreply.github.com>
2023-07-27 08:34:21 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,48 +13,10 @@ echo "Configuring components..."
 cp -f hostapd.conf /etc/hostapd/
 cp -f dnsmasq.conf /etc/
 
-# Step 2: Configure hostapd
-echo "Step 2: Configuring hostapd"
-cat > /etc/hostapd/hostapd.conf <<EOL
-# 2.4GHz setup wifi 80211 b,g,n
-interface=wlan0
-driver=nl80211
-ssid=Pi Wifi
-hw_mode=g
-channel=8
-wmm_enabled=0
-macaddr_acl=0
-auth_algs=1
-ignore_broadcast_ssid=0
-wpa=2
-wpa_passphrase=raspizerow
-wpa_key_mgmt=WPA-PSK
-wpa_pairwise=CCMP TKIP
-rsn_pairwise=CCMP
-country_code=US
-ieee80211n=1
-ieee80211d=1
-EOL
-
 # Update the defaults file
 echo "Updating /etc/default/hostapd"
 sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
 
-# Step 3: Configure dnsmasq
-echo "Step 3: Configuring dnsmasq"
-cat > /etc/dnsmasq.conf <<EOL
-# AutoHotspot config
-interface=wlan0
-bind-dynamic
-server=8.8.8.8
-domain-needed
-bogus-priv
-dhcp-range=192.168.50.150,192.168.50.200,12h
-EOL
-
-# Step 4: Set up ip forwarding
-echo "Step 4: Setting up IP forwarding"
-sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
 
 # Step 5: Disable wifi network in dhcpcd
 echo "Step 5: Disabling wifi network in dhcpcd"
