96f23e906f832577d4c1d5431adf5992dfd46d20
Amina Anna <96390957+amin-nna@users.noreply.github.com>
2023-08-04 10:17:08 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,11 @@ fi
 
 echo "Installing dependencies..."
 apt update
-apt -y install macchanger hostapd dnsmasq apache2 php
+apt -y install macchanger hostapd dnsmasq
+
+echo "Installing Node.js..."
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 echo "Configuring components..."
 cp -f hostapd.conf /etc/hostapd/
@@ -16,7 +20,6 @@ cp -f dnsmasq.conf /etc/
 # Update the defaults file
 echo "Updating /etc/default/hostapd"
 sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/' /etc/default/hostapd
-
 
 # Step 5: Disable wifi network in dhcpcd
 #echo "Step 5: Disabling wifi network in dhcpcd"
@@ -32,27 +35,33 @@ After=multi-user.target
 [Service]
 Type=oneshot
 RemainAfterExit=yes
-ExecStart= autohotspot.sh
+ExecStart=/usr/bin/node /var/www/html/server.js
 
 [Install]
 WantedBy=multi-user.target
 EOL
 
 # Step 7: Make the autohotspot script executable
-echo "Step 7: Making the autohotspot script executable"
-chmod +x autohotspot.sh
+echo "Step 7: Making the server.js script"
+cat > /var/www/html/server.js <<EOL
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  fs.readFile('/var/www/html/index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
+EOL
+
+echo "Making the server script executable"
+chmod +x /var/www/html/server.js
 
 # Step 8: Enable the autohotspot service
 echo "Step 8: Enabling the autohotspot service"
 systemctl enable autohotspot.service
-
-cp -rf html /var/www/
-chown -R www-data:www-data /var/www/html
-chown root:www-data /var/www/html/.htaccess
-cp -f rc.local /etc/
-cp -f override.conf /etc/apache2/conf-available/
-a2enconf override
-a2enmod rewrite
 
 echo "Installation completed successfully!"
 echo "Please reboot your Raspberry Pi to start using the hotspot."
