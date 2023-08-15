a63fe257fff6c4f514af8874864b98ebe4121c2b
Amina Anna <96390957+amin-nna@users.noreply.github.com>
2023-08-14 22:16:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ apt update
 apt -y install macchanger hostapd dnsmasq
 
 echo "Installing Node.js..."
-curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 echo "Configuring components..."
