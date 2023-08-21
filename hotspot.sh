e7bda94799c11b5776fd5350ac073117fd5ec79f
Amina Anna <96390957+amin-nna@users.noreply.github.com>
2023-08-21 09:53:41 -0400

    Update hotspot.sh

--- hotspot.sh
+++ hotspot.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-# use the settings you provided for the hotspot
+# Use the settings you provided for the hotspot
 cp -f hostapd.conf /etc/hostapd/
 cp -f dnsmasq.conf /etc/
 service hostapd start
 service dnsmasq start
-
 echo "Hotspot started"
 exit 0
