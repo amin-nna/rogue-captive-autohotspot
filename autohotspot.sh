558b86bfe923e3449cafdafc04b9bcf1e88fd3d8
Amina Anna <96390957+amin-nna@users.noreply.github.com>
2023-08-21 09:52:43 -0400

    Update autohotspot.sh

--- autohotspot.sh
+++ autohotspot.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-
+# Start wpa_supplicant
+wpa_supplicant -B -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant.conf
 # Use wpa_cli to reconfigure wpa_supplicant and try to connect to known networks
 wpa_cli -i wlan0 reconfigure
-
 # Sleep for 15 seconds to give time for connection
 sleep 15
-
-# get the currently connected WiFi network
+# Get the currently connected WiFi network
 connected_ssid=$(iwgetid -r)
-
-# check if we're connected to a WiFi network
+# Check if we're connected to a WiFi network
 if [[ -n $connected_ssid ]]; then
     echo "Connected to network $connected_ssid"
     exit 0
 else
-    # if we're not connected to a known network, create a hotspot
+    # If we're not connected to a known network, create a hotspot
     echo "Creating hotspot..."
     sh hotspot.sh
 fi
-
 exit 0
+
